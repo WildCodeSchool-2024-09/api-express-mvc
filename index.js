8ca8b68207b4e11073a0891274cf983d95f1c62c
yavuzkutuk <yavuz@kutuk.fr>
2024-12-09 16:20:27 +0100

    Api Express MVC

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(`${__dirname}/public`));
 routes(app);
 
-// La logique V du MVC
+// La logique V (views, endpoint, route) du MVC
 app.get('/exemple', (req, res) => {
-  // La logique M du MVC
+  // La logique M(models, data, sql) du MVC
   const data = 'je suis la route exemple';
 
-  // La logique C du MVC
+  // La logique C(controllers, traitement) du MVC
   return res.send({
     message: data,
   });
